3a409c01410380644a8db69f8670cee5cf31b1f0
aliqays1 <aliqays76@gmail.com>
2026-05-29 22:27:42 +0300

    fix: force ipv4 for nodemailer to fix ENETUNREACH on render

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
